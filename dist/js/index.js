e39ab9645c77f999402255ca9d454a9da42085c6
Leonel Cassul <leonelsalmos@gmail.com>
2023-05-01 15:57:06 +0100

    Criaçao de variaveis

---
+++ dist/js/index.js
@@ -0,0 +1,17 @@
+"use strict";
+/* Como criar variaveis */
+/* Sintaxe: identificador(var (global), const(nao podemos reatribuir) e let( escopo local) nome da variavel */
+let num = 1369;
+console.log(num);
+// Tipos basico
+let firstName = "Leonel";
+let age = 28;
+console.log(firstName);
+console.log(typeof firstName);
+// Object
+const myNumber = [1, 3, 4, 5];
+console.log(myNumber);
+console.log(myNumber.length);
+console.log(firstName.toUpperCase());
+myNumber.push(6);
+console.log(myNumber);
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+ /* Como criar variaveis */
+  /* Sintaxe: identificador(var (global), const(nao podemos reatribuir) e let( escopo local) nome da variavel */
+
+let num: number = 1369;
+console.log(num);
+
+// Tipos basico
+
+let firstName: string = "Leonel";
+let age:number = 28
+console.log(firstName);
+console.log(typeof firstName);
+
+// Object
+const myNumber: number[] = [1,3,4,5];
+
+console.log(myNumber)
+console.log(myNumber.length)
+console.log(firstName.toUpperCase())
+myNumber.push(6)
+console.log(myNumber)
